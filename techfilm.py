0869694783bc344ccab825c227e0db4b05a4bdcf
Nhi Tran <95414776+tranpnhi@users.noreply.github.com>
2023-01-18 14:26:48 +0700

    Create techfilm.py

---
+++ techfilm.py
@@ -0,0 +1,9 @@
+import requests
+
+requests.post("https://api.ai21.com/studio/v1/experimental/rewrite",
+    headers={"Authorization": "Bearer YOUR_API_KEY"},
+    json={
+        "text": "Good writing is about finding the best words to convey your message",
+        "intent": "general"
+    }
+)
