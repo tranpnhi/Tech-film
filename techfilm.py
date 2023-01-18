eb82fd21c07ff4218812a08681081b9eb31604e2
Nhi Tran <95414776+tranpnhi@users.noreply.github.com>
2023-01-18 14:53:02 +0700

    Update techfilm.py

--- techfilm.py
+++ techfilm.py
@@ -5,5 +5,6 @@ requests.post("https://api.ai21.com/studio/v1/experimental/rewrite",
     json={
         "text": "Good writing is about finding the best words to convey your message",
         "intent": "general"
+        #there are 5 intents (tones): general, long,short,formal,casual
     }
 )
